f7fcf480479210567c4b66e14a9001f2007a0fcd
lucasconsejo <lucas.consejo@ynov.com>
2021-03-18 13:45:38 +0100

    🎉 Initial commit

---
+++ OfferConfigurator/Databases/OfferConfiguratorDatabaseSettings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace OfferConfigurator.Databases
+{
+    public class OfferConfiguratorDatabaseSettings : IOfferConfiguratorDatabaseSettings
+    {
+        public string ProductsCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IOfferConfiguratorDatabaseSettings
+    {
+        string ProductsCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+}
+
---
+++ OfferConfigurator/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace OfferConfigurator.Models
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+        public string Option { get; set; } // TODO: Enum je pense
+    }
+}
